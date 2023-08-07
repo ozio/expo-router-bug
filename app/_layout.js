3ab8b5810614a4033207f6a3c62f84842c65a34b
Nikolay Solovyov <i@mr-ozio.ru>
2023-08-07 10:34:40 +0400

    initial

--- app/_layout.js
+++ app/_layout.js
@@ -3,13 +3,13 @@ import { Stack } from "expo-router";
 export default function Layout() {
   return (
     <Stack>
-      {/* if you uncomment following tag it can't go deep with animation: */}
-      <Stack.Screen
+      {/* if you uncomment following tag it will break the default behavior: */}
+      {/* <Stack.Screen
         name="[id]"
         options={{
           title: "title",
         }}
-      />
+      /> */}
     </Stack>
   );
 }
